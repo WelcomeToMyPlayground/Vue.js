85b0db959a8522c80bb1383b1b8c9686bb97c903
Kim Yerin <yyr9530@naver.com>
2024-01-08 14:30:42 +0900

    0108

--- 쀼/vuedongsan/src/App.vue
+++ 쀼/vuedongsan/src/App.vue
@@ -1,13 +1,22 @@
 <template>
+
+  <div class="menu">
+    <a v-for="작명 in 메뉴들" :key="작명">Home</a>
+
+  </div>
+
   <img alt="Vue logo" src="./assets/logo.png">
   <div>
-    원룸샵
-    <h4 class="red" :style="스타일">XX 원룸</h4>
-    <p>{{ price1 }} 만원</p>
+    <h4>{{ products[0] }}</h4>
+    <p>{{ price1 }}만원</p>
   </div>
   <div>
-    <h4>XX 원룸</h4>
-    <p>{{ price2 }} 만원</p>
+    <h4>{{ products[1] }}</h4>
+    <p>{{ price2 }}만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>{{ price3 }}만원</p>
   </div>
 </template>
 
@@ -17,9 +26,10 @@ export default {
   name: 'App',
   data(){
     return {
-      price1 : 60,
-      price2 : 70,
-      스타일 : 'color : blue',
+      price1 : 50,
+      price2 : 60,
+      price3 : 70,
+      메뉴들 : ['Home','Shop','About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
       // 자료이름 : 자료 내용
     }
@@ -37,5 +47,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
