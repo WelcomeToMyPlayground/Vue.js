2eff1656dd3123bc28aee6c82fc0ddb61dc2a17f
Kim Yerin <yyr9530@naver.com>
2024-02-21 01:28:39 +0900

    .

--- test/src/Modal.vue
+++ test/src/Modal.vue
@@ -6,7 +6,9 @@
       <img :src="원룸들[누른거].image" style="width:100%">
       <h4>{{ 원룸들[누른거].title}}</h4>
       <p>상품 설명 : {{ 원룸들[누른거].content}}</p>
-      <p>가격 : {{ 원룸들[누른거].price }}원</p>
+      <input v-model.number ="month">
+      <!-- <input @input="month = $event.target.value"> -->
+      <p>{{ month }}개월 선택함 : {{ 원룸들[누른거].price + month }}원</p>
       <button @onclick="$emit('closeModal')">닫기</button>
       <!-- <button v-on:click="modal = false">닫기</button> -->
     </div>
@@ -17,6 +19,12 @@
 <script>
 export default {
   name : 'Modal',
+  data(){
+    return{
+      month : 1,
+    
+    }
+  },
   props: {
     원룸들 : Array,
     누른거 : Number,
